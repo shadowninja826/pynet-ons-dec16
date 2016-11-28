1e352a61f191110e36d7d4b1f9154197b414d956
shadowninja826 <sodachheng@gmail.com>
2016-11-28 09:27:56 -0800

    added str ex 1

---
+++ str_ex1.py
@@ -0,0 +1,10 @@
+name1 = "Mickey Mouse"
+name2 = "Ken Master"
+name3 = "Super Mario"
+name4 = raw_input("enter a name: ")
+
+print
+print "{:<30}".format(name1)
+print "{:<30}".format(name2)
+print "{:<30}".format(name3)
+print "{:<30}".format(name4)
